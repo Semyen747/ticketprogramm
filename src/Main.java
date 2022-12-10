51cc57e483f36e155c7c918492819bf9645bb1e2
Semyen747 <combatpilot45@gmail.com>
2022-12-10 11:37:44 +0300

    add java file

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        float ticketPrice = 2000;
+        int milePrice = 20;
+        int milesCost = (int) ticketPrice / milePrice;
+        System.out.println(milesCost);
+        System.out.println("milesCost");
+    }
+}
